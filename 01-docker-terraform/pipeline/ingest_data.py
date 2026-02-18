7505506d6f32314bce8c65b0b0088661c53015b3
Kuochh <79375430+kuochh@users.noreply.github.com>
2026-02-18 22:34:11 +0000

    finished module 02

---
+++ 01-docker-terraform/pipeline/ingest_data.py
@@ -0,0 +1,68 @@
+import click
+import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
+    """Ingest NYC taxi data into PostgreSQL."""
+    
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+    url = prefix + 'yellow_tripdata_2021-01.csv.gz'
+    
+    engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+    
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=100000
+    )
+    
+    first_chunk = next(df_iter)
+    
+    first_chunk.head(0).to_sql(name=target_table, con=engine, if_exists='replace')
+    print(f"Created table {target_table}")
+    
+    first_chunk.to_sql(name=target_table, con=engine, if_exists='append')
+    print(f"Inserted first chunk: {len(first_chunk)} rows")
+    
+    for df_chunk in tqdm(df_iter):
+        df_chunk.to_sql(name=target_table, con=engine, if_exists='append')
+
+
+if __name__ == '__main__':
+    run()
